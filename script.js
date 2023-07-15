7e8f03907d372bf5e1b6eec2bdde05fce4565a64
Hazem K Ali <hazemkhaled.work@gmail.com>
2023-07-15 04:42:17 +0300

    Add Play button and result functinality
    + play button compare picks and shows the result when clicked
    
    + add a div to contain the score announcement, what happened and why
    
    + add styling to the spans containing picks,result announcement
    , player score and computer score

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const COMPUTER_POSSIBILITIES = ["Rock","Paper","Scissors"] ;
 
-
 // ****** Function to generate computer pick ******
 let computerSelection = getComputerChoice () ;
 function getComputerChoice () {
     return COMPUTER_POSSIBILITIES[Math.floor(Math.random()*
         (COMPUTER_POSSIBILITIES.length))] ;
     }
-// console.log(computerSelection); test## make sure getComputerChoice function work
+// console.log(computerSelection); //test## make sure getComputerChoice function work
 
 
 
@@ -16,15 +15,97 @@ let playerSelection ;
 let rockButton = document.querySelector("#rock-button");
 let paperButton = document.querySelector("#paper-button");
 let scissorsButton = document.querySelector("#scissors-button");
-// console.log(scissorsButton);  test## make sure button are assigned to variables
+// console.log(scissorsButton);  //test## make sure button are assigned to variables
 
-rockButton.addEventListener('click', ()=> { playerSelection="Rock" ;
-return console.log(playerSelection);})
+rockButton.addEventListener('click', ()=> { 
+return playerSelection="Rock" ;})
 
-paperButton.addEventListener('click', ()=> { playerSelection="Paper" ;
-return console.log(playerSelection);})
+paperButton.addEventListener('click', ()=> { 
+return playerSelection="Paper" ;})
 
-scissorsButton.addEventListener('click', ()=> { playerSelection="Scissors" ;
-return console.log(playerSelection);})
+scissorsButton.addEventListener('click', ()=> { 
+return playerSelection="Scissors" ;})
 
+
+
+
+
+
+
+
+// ****** Play button function ******
+let playButton = document.querySelector("#play-button");
+let resultSpace = document.querySelector('#decalre-result') ;
+let declarePlayerPick = document.querySelector('#decaler-player-pick') ;
+let declareComputerPick = document.querySelector('#decaler-computer-pick') ;
+
+    // test## activate the 2 line below,pick your hand and press play to test me
+    // console.log("You selected "+playerSelection);
+    // console.log("computer selected "+computerSelection);
+
+
+playButton.addEventListener('click',() => {
+    //declare picks section (so the player can see what was picked) 
+    declarePlayerPick.innerHTML=`${playerSelection}`;
+    declareComputerPick.innerHTML=`${computerSelection}`;
+
+
+    
+    // we have 7 total cases 
+
+    // Draw case document.querySelector('#decalre-reult') ;
+    if (computerSelection === playerSelection) {
+        resultSpace.innerHTML=`Draw , 
+        computer also picked ${computerSelection}`
+    }
+
+    // picks are different - computer chose Rock
+    if (computerSelection === "Rock") {
+
+        switch (playerSelection) {
+
+            case "Paper":
+            resultSpace.innerHTML=`Player's Paper beats computer's Rock`;
+            break;
+
+            case "Scissors":
+            resultSpace.innerHTML=`Player's Scissors loses to computer's Rock`;
+            break;
+
+        }
+    }
+
+    // picks are different - computer chose Paper
+    if (computerSelection === "Paper") {
+
+        switch (playerSelection) {
+
+            case "Rock":
+            resultSpace.innerHTML=`Player's Rock loses to computer's Paper`;
+            break;
+
+            case "Scissors":
+            resultSpace.innerHTML=`Player's Scissors beats  computer's Rock`;
+            break;
+
+        }
+    }
+
+    // picks are different - computer chose Scissors
+    if (computerSelection === "Scissors") {
+
+        switch (playerSelection) {
+
+            case "Rock":
+            resultSpace.innerHTML=`Player's Rock beats  computer's Scissors`;
+            break;
+
+            case "Paper":
+            resultSpace.innerHTML=`Player's Paper loses to computer's Scissors`;
+            break;
+
+        }
+    }
+
+})
 
