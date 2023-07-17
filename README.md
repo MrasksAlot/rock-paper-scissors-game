7bc34f3e956407470a05385ea6bd888d4deb1dea
Hazem K Ali <hazemkhaled.work@gmail.com>
2023-07-17 07:29:10 +0300

    Add a score tracker  & finalize project
    This will be a big commit i did the following
    
    - organize the code into 4 sections
    variables decleration, computer pick generator, player pick buttons
    and finally the play button even and its functionality
    
    - add a score tracker and result decleration
    
    - in case player didn't pick a button before pressing play
    an alert will be displayed and computer pick and player pick massages
    will urge the player to pick something

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# rock-paper-scissors-game
+#  rock-paper-scissors-game 
+## by Hazem Ali
+
+### Livelink below, constructive feedback is Welcomed
+[Rock Paper Scissors Game](https://mrasksalot.github.io/rock-paper-scissors-game/)
+| ------------------------------- |
+
+## Features
+
+##### - If no hand was picked throw an alert to notify the user and show msg to urge the user to pick
+
+##### - First to 5 score tracker
+
+##### - Decalers results and update score on every play button press
+
+##### - Responsive to smaller screens/viewports
+
+##### - Code is organized into 4 main sections with the 4th being the largest 
+(as it contains most of the functionality and the score tracker)
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+// ****** 1) General Variables deceleration ******
+
 const COMPUTER_POSSIBILITIES = ["Rock","Paper","Scissors"] ;
 let computerSelection ;
 
-// ****** Function to generate computer pick ******
+let playButton = document.querySelector("#play-button");
+let resultSpace = document.querySelector('#decalre-result') ;
+let declarePlayerPick = document.querySelector('#decaler-player-pick') ;
+let declareComputerPick = document.querySelector('#decaler-computer-pick') ;
+    // test## activate the 2 line below,pick your hand and press play to test me
+    // console.log("You selected "+playerSelection);
+    // console.log("computer selected "+computerSelection);
+
+winOrLoseMsg = document.querySelector(`#win-or-lose`) ;
+
+let playerSelection ; 
+let rockButton = document.querySelector("#rock-button");
+let paperButton = document.querySelector("#paper-button");
+let scissorsButton = document.querySelector("#scissors-button");
+    // console.log(scissorsButton);  //test## make sure button are assigned to variables
+
+let playerScore= 0;
+let computerScore= 0;
+
+playerScoreValue= document.querySelector(`#player-score`) ;
+computerScoreValue= document.querySelector(`#computer-score`) ;
+
+// ******End of Variables deceleration ******
+
+
+
+
+
+// ****** 2) The Function to generate computer pick ******
+
 function getComputerChoice () {
     return COMPUTER_POSSIBILITIES[Math.floor(Math.random()*
         (COMPUTER_POSSIBILITIES.length))] ;
     }
 
+// ****** End of the Function to generate computer pick ******
 
 
-// ****** Getting play pick using the buttons ******
-let playerSelection ; 
-let rockButton = document.querySelector("#rock-button");
-let paperButton = document.querySelector("#paper-button");
-let scissorsButton = document.querySelector("#scissors-button");
-// console.log(scissorsButton);  //test## make sure button are assigned to variables
+
+
+
+// ****** 3) Getting player pick using the buttons ******
 
 rockButton.addEventListener('click', ()=> { 
 return playerSelection="Rock" ;})
@@ -25,38 +55,32 @@ return playerSelection="Paper" ;})
 scissorsButton.addEventListener('click', ()=> { 
 return playerSelection="Scissors" ;})
 
+// ****** End of Getting play pick using the buttons ******
 
 
 
 
 
-// ****** Play button function ******
-let playButton = document.querySelector("#play-button");
-let resultSpace = document.querySelector('#decalre-result') ;
-let declarePlayerPick = document.querySelector('#decaler-player-pick') ;
-let declareComputerPick = document.querySelector('#decaler-computer-pick') ;
-
-    // test## activate the 2 line below,pick your hand and press play to test me
-    // console.log("You selected "+playerSelection);
-    // console.log("computer selected "+computerSelection);
-
+// ****** 4) Play button event (What happens when you press play) ******
 
 playButton.addEventListener('click',() => {
-if (playerSelection===undefined){
+
+    
+    while (playerSelection===undefined){
     alert("Kindly choose your hand then press play to avoid Undefined appearing as your pick");
     
+    playerSelection = "Still waiting for you to choose "
+    computerSelection = "Computer won't play alone!, pick a hand"
+        //declare picks section (in case of player not choosing) 
+        declarePlayerPick.innerHTML=`${playerSelection}`;
+        declareComputerPick.innerHTML=`${computerSelection}`;
+    return
+
 }
 
     //generate random computer choice everytime play button is clicked
     computerSelection = getComputerChoice () ;
 
-    //declare picks section (so the player can see what was picked) 
-    declarePlayerPick.innerHTML=`${playerSelection}`;
-    declareComputerPick.innerHTML=`${computerSelection}`;
-
-
-
-    
     // we have 7 total cases 
 
     // Draw case document.querySelector('#decalre-reult') ;
@@ -66,7 +90,7 @@ if (playerSelection===undefined){
     }
 
     // picks are different - computer chose Rock
-    if (computerSelection === "Rock") {
+    else if (computerSelection === "Rock") {
 
         switch (playerSelection) {
 
@@ -82,7 +106,7 @@ if (playerSelection===undefined){
     }
 
     // picks are different - computer chose Paper
-    if (computerSelection === "Paper") {
+    else if (computerSelection === "Paper") {
 
         switch (playerSelection) {
 
@@ -97,7 +121,7 @@ if (playerSelection===undefined){
     }
 
     // picks are different - computer chose Scissors
-    if (computerSelection === "Scissors") {
+    else if (computerSelection === "Scissors") {
 
         switch (playerSelection) {
 
@@ -111,19 +135,60 @@ if (playerSelection===undefined){
 
         }
     }
+    declarePlayerPick.innerHTML=`${playerSelection}`;
+    declareComputerPick.innerHTML=`${computerSelection}`;
+
+        // ******  First to get 5 points Score tracker ******
+
+            //decalre current score
+            playerScoreValue.innerHTML=`  ${playerScore}`;
+            computerScoreValue.innerHTML=`  ${computerScore}`;
+
+            //update score values based on results
+            if (playerSelection === "Rock") {
+                if (computerSelection === "Paper") {++computerScore ;}
+                else if (computerSelection === "Scissors") {++playerScore ;}
+                playerScoreValue.innerHTML=`  ${playerScore}`;
+                computerScoreValue.innerHTML=`  ${computerScore}`;
+            }
+            if (playerSelection === "Paper") {
+                if (computerSelection === "Scissors") {++computerScore ;}
+                else if (computerSelection === "Rock") {++playerScore ;}
+                playerScoreValue.innerHTML=`  ${playerScore}`;
+                computerScoreValue.innerHTML=`  ${computerScore}`;
+            }
+            if (playerSelection === "Scissors") {
+                if (computerSelection === "Rock") {++computerScore ;}
+                else if (computerSelection === "Paper") {++playerScore ;}
+                playerScoreValue.innerHTML=`  ${playerScore}`;
+                computerScoreValue.innerHTML=`  ${computerScore}`;
+            }
+
+            if ( playerScore ===5 || computerScore===5 ) {
+
+                if (playerScore ===5) { 
+                    //declare the winner
+                    resultSpace.innerHTML=`"PLayer won this round" ${playerScore} to ${computerScore} ` ;
+                    //reseting score
+                    playerScore = 0 ;
+                    computerScore = 0 ;
+            }
+                else if (computerScore ===5) { 
+                    //declare the winner
+                    resultSpace.innerHTML=`"Computer won this round" ${computerScore} to ${playerScore} ` ;
+                    //reseting score
+                    playerScore = 0 ;
+                    computerScore = 0 ;
+            }
+
+            }
 })
 
-// ****** Best out of 5 Score tracker ******
-winOrLoseMsg = document.querySelector(`#win-or-lose`) ;
-let playerScore= 0;
-let computerScore= 0;
+// ****** End of Play button event (What happens when you press play) ******
 
 
-if (computerSelection === "Rock") {
-    if (playerSelection === "Paper") {++playerScore ;}
-    else if (playerSelection === "Scissors") {++computerScore}
-}
-document.querySelector(`#player-score`).innerHTML=`  ${playerScore}`;
-document.querySelector(`#computer-score`).innerHTML=`  ${computerScore}`;
+
+
+
 
 
